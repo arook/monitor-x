bb2ade400e3bd01060ebb8d0b4d5f6839f0895f9
arook <arook@vip.qq.com>
2011-12-07 17:28:11 +0800

    1

--- protected/views/site/bbr.php
+++ protected/views/site/bbr.php
@@ -1,6 +1,19 @@
 
-<?php
-foreach($buybox as $item) {
-  echo $item['seller'];
-}
-?>
+<table style="width:900px;overflow:auto;display:block;">
+<tr>
+  <th>Seller</th>
+  <th>FBA</th>
+  <th>TYPE</th>
+  <th>Rate</th>
+</tr>
+<tbody>
+<?php foreach($buybox as $item):?> 
+<tr>
+  <td><?php echo $item['seller'] ?></td>
+  <td><?php echo $item['if_fba'] ?></td>
+  <td><?php echo $item['type'] ?></td>
+  <td><?php echo 100 * $item['rate'] ?> %</td>
+</tr>
+<?php endforeach;?>
+</tbody>
+</table>
