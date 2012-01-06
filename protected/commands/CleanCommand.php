9561461cfb0fe8b1b94c37a010dcd26a346607d6
arook <arook@vip.qq.com>
2012-01-06 17:12:04 +0800

    14 day

--- protected/commands/CleanCommand.php
+++ protected/commands/CleanCommand.php
@@ -6,7 +6,7 @@ class CleanCommand extends CConsoleCommand {
     $result = Yii::app()->db->createCommand('
         SELECT * 
         FROM  `fetching` 
-        WHERE  `dt` < DATE_SUB( NOW( ) , INTERVAL 1 MONTH )
+        WHERE  `dt` < DATE_SUB( NOW( ) , INTERVAL 14 DAY )
       ')->query();
 
     if ($result->getRowCount()) {
