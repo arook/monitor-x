a7c73780460666161b60025e6aef7b82357a0651
arook <arook@vip.qq.com>
2012-01-06 17:05:43 +0800

    clean

---
+++ protected/commands/CleanCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+class CleanCommand extends CConsoleCommand {
+
+  public function run($args) {
+    $result = Yii::app()->db->createCommand('
+        SELECT * 
+        FROM  `fetching` 
+        WHERE  `dt` < DATE_SUB( NOW( ) , INTERVAL 1 MONTH )
+      ')->query();
+
+    if ($result->getRowCount()) {
+      while ($fetching = $result->read()) {
+        echo $fetching['id'];
+        $this->deleteFetching($fetching['id']);
+      }
+    }
+
+  }
+
+  private function deleteFetching($id) {
+    Yii::app()->db->createCommand("
+      delete from `fetching_issue` where `fetching_id` = '$id'
+      ")->query();
+    Yii::app()->db->createCommand("
+      delete from `fetching_detail` where `fetching_id` = '$id'
+      ")->query();
+    Fetching::model()->deleteByPk($id);
+  }
+}
+?>
