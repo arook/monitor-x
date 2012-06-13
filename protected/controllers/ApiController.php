1cc04c708df212a07d6734998771d77007e46b0d
arook <arook@vip.qq.com>
2012-06-13 13:15:33 +0800

    webservice

---
+++ protected/controllers/ApiController.php
@@ -0,0 +1,66 @@
+<?php
+
+class ApiController extends Controller
+{
+
+	public function actions()
+	{
+		return array(
+			'index'=>array(
+				'class'=>'CWebServiceAction',
+			),
+		);
+	}
+
+  /**
+   * @param string the asin to look up
+   * @return array list
+   * @soap
+   */
+  public function getListing($asin) {
+    $aid = Redis::client()->hget('asins', $asin);
+    if(!$aid)
+      return null;
+    $fid = Redis::client()->lrange("asin:{$aid}:fetch", -1, -1);
+    if(!$fid)
+      return null;
+    $list = Redis::client()->lrange("fetch:{$fid[0]}:list", 0, -1);
+    foreach($list as &$item) {
+      $item = CJSON::decode($item);
+      $item['seller'] = $this->getSellerName($item['sid']);
+    }
+    return $list;
+  }
+
+  /**
+   * @param array asins to look up
+   * @return array list list
+   * @soap
+   */
+  public function getListingList($asins) {
+    $list = array();
+    foreach($asins as $asin) {
+      $list[$asin] = $this->getListing($asin);
+    }
+    return $list;
+  }
+
+  /**
+   * @param string the asin to add
+   * @return bool if added successed
+   * @soap
+   */
+  public function addAsin($asin) {
+    if(!Redis::client()->hexists('asins', $asin)) {
+      $aid = Redis::client()->incr('ids:nextAsin');
+      Redis::client()->hset('asins', $asin, $aid);
+      return true;
+    }
+    return false;
+  }
+
+  private function getSellerName($sid) {
+    $skey = Redis::client()->hget('sellers', $sid);
+    return Redis::client()->get("seller:{$skey}:name");
+  }
+}
