eee0838fda267f8b86cf7160b6a64d6f08bca559
arook <arook@vip.qq.com>
2012-06-21 11:31:54 +0800

    1

--- protected/components/AsinService.php
+++ protected/components/AsinService.php
@@ -28,20 +28,75 @@ class AsinService extends CComponent {
   private function __clone() {
   }
 
+  /**
+   * 从原始数据计算给定ASIN对没一个卖家的BBR，并保存
+   * 长key = bbr:{$aid}:{$sid}:{$level}
+   */
   public function bbrComputeFromSource($aid) {
     list($b1, $b2, $b3) = array(
       Yii::app()->params['fetch_frequency'] * 12,
       Yii::app()->params['fetch_frequency'] * 36,
       Yii::app()->params['fetch_frequency'] * 84
     );
+    $bbrs = array('1'=>array(), '2'=>array(), '3'=>array());
     $fetches = self::$_client->lrange("asin:{$aid}:fetch", 0, $b3);
     foreach($fetches as $key=>$fid) {
-      $list = self::$_client->lrange("fetch:{$fid}:list", 0, 100);
-      foreach($list as $entity) {
-        echo $entity, "\n";
+      $list = self::$_client->lrange("fetch:{$fid}:list", 0, -1);
+      foreach($list as $key=>$entity) {
+        $tmp = CJSON::decode($entity);
+        if($tmp['if_buybox']) {
+          if(array_key_exists($tmp['sid'], $bbrs[3])) {
+            $bbrs[3][$tmp['sid']] += 1;
+          } else {
+            $bbrs[3][$tmp['sid']] = 1;
+          }
+        }
+
+        if($key == $b1) {
+          $bbrs[1] = $bbrs[3];
+        }
+        if($key == $b2) {
+          $bbrs[2] = $bbrs[3];
+        }
       }
     }
-      //
+
+    print_r($bbrs);
+    return;
+    $this->bbrSet('bbr:12345678:AXXXX:1', 101);
+    echo $this->bbrGet('bbr:12345678:AXXXX:1');
+    return;
+  }
+
+  /**
+   * 储存指定ASIN ID，指定SELLER ID，指定LEVEL的BBR
+   *
+   * @params string $key bbr:{$aid}:{$sid}:{$level}
+   * @params int $aid
+   * @params int $sid
+   * @params int $level in 1, 2, 3
+   */
+  public function bbrSet($key, $val) {
+    list($k, $f) = $this->bbrKeyField($key);
+    self::$_client->hset($k, $f, $val);
+  }
+
+  public function bbrGet($key) {
+    list($k, $f) = $this->bbrKeyField($key);
+    return self::$_client->hget($k, $f);
+  }
+
+  /**
+   * 内存优化，将bbr平均分布
+   *
+   * @params string $key
+   * @return array array(key, field)
+   */
+  private function bbrKeyField($key) {
+    list(,$aid,$sid,$level) = explode(':', $key);
+    $k = 'bbr:' . substr($aid, 0, -2);
+    $f = substr($aid, -2) . ':' .$sid . ':' . $level;
+    return array($k, $f);
   }
 
   public function bbrComputeFromDiff($aid) {
