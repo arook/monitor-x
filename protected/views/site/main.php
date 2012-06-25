e5bf9bde5e381274f78c4cb60719ce4013ad2ff0
arook <arook@vip.qq.com>
2012-06-25 14:19:53 +0800

    xx

--- protected/views/site/main.php
+++ protected/views/site/main.php
@@ -7,18 +7,23 @@
 <table>
 <thead>
   <th>SELLER</th>
-  <th>1</th>
-  <th>3</th>
-  <th>5</th>
+  <th>12</th>
+  <th>36</th>
+  <th>84</th>
+  <th>12</th>
+  <th>36</th>
+  <th>84</th>
 </thead>
 <?php foreach($bbr as $key=>$seller):?>
 <tr>
   <td><?php echo Utils::getAvatarBySid($key);?></td>
-  <td>
-  <?php echo $sales[$key]['1']?>
-<?php echo sprintf('%.2f%%', $seller['1'] * 100)?></td>
+  <td><?php echo sprintf('%.2f%%', $seller['1'] * 100)?></td>
   <td><?php echo sprintf('%.2f%%', $seller['2'] * 100)?></td>
   <td><?php echo sprintf('%.2f%%', $seller['3'] * 100)?></td>
+
+  <td><?php echo sprintf('%.2f', $sales[$key]['1'])?></td>
+  <td><?php echo sprintf('%.2f', $sales[$key]['2'])?></td>
+  <td><?php echo sprintf('%.2f', $sales[$key]['3'])?></td>
 </tr>
 <?php endforeach;?>
 </table>
