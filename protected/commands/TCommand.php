6208ab64d64abd8df518c536eb77f3b0a12a6d61
arook <arook@vip.qq.com>
2013-03-25 18:14:18 +0800

    add pricing and bbx support

--- protected/commands/TCommand.php
+++ protected/commands/TCommand.php
@@ -2,9 +2,19 @@
 
 class TCommand extends CConsoleCommand {
   public function run() {
-    $node = MNode::model()->findByAttributes(array('id'=>"1"));
-    $node->lock();
+    // $f =  MFetching::model()->find();
+    // foreach ($f['l'] as $key => $value) {
+    //   var_dump($value['s']['$id']->{'$id'});
+    // }
+    // return;
     
-    // $node->unlock();
+    $client = new SoapClient('http://monitor/?r=api');
+    try {
+      // $resp = $client->getPricing('B002DPO222', 24);
+      $resp = $client->getBbxPricing('B002DPO222', 24);
+      var_dump($resp);
+    } catch (Exception $e) {
+      var_dump($e);
+    }
   }
 }
--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -2,6 +2,8 @@
 
 class ApiController extends Controller
 {
+  
+  private $_seller_list = array();
 
 	public function actions()
 	{
@@ -54,5 +56,74 @@ class ApiController extends Controller
       $list[$asin] = $this->getListing($asin);
     }
     return $list;
+  }
+  
+  /**
+   * 获取指定asin的过去多少个小时的价格曲线
+   * @param string the asin to look up
+   * @param int hours
+   * @return array list
+   * @soap
+   */
+  public function getPricing($asin, $hours = 24)
+  {
+    $list = array();
+		$fetching = $this->getListByAsin($asin, $hours);
+		
+		foreach ($fetching as $key => $f) {
+		  foreach($f['l'] as $row) {
+        $seller = $this->getSeller($row['s']);
+        $list[$key][] = array(
+          // 'bbx' => $f['br'],
+          'rank' => $row['r'],
+          'seller' => $seller['name'],
+          'if_fba' => $row['f'],
+          'sell_price' => $row['p'],
+          'shipping_price' => $row['sp'],
+        );
+      }
+		}
+    return $list;
+  }
+  
+  /**
+   * 获取指定ASIN的BBX价格
+   * @param string the asin to look up
+   * @param int hours
+   * @return array list
+   * @soap
+   */
+  public function getBbxPricing($asin, $hours = 24)
+  {
+    $list = array();
+    $fetching = $this->getListByAsin($asin, $hours);
+    
+    foreach ($fetching as $key => $f) {
+      if ($f['br']) {
+        $list[] = $f['l'][$f['br'] - 1]['p'] + $f['l'][$f['br'] - 1]['sp'];
+      }
+    }
+    return $list;
+  }
+  
+  private function getListByAsin($asin, $hours)
+  {
+    $masin = MAsin::model()->findByAttributes(array('asin'=>$asin));
+    if (!$masin)
+      return;
+    $c = MFetching::model()->getDbCriteria()
+			->addCond('a.$id', '==', $masin->_id)
+			->addCond('status', '==', 200)
+			->addCond('t', '>', new MongoDate(strtotime("- $hours hours")))
+			->sort('t', -1);
+		return MFetching::model()->findAll($c);
+  }
+  
+  private function getSeller($sid)
+  {
+    if (!array_key_exists($sid['$id']->{'$id'}, $this->_seller_list)) {
+      $this->_seller_list[$sid['$id']->{'$id'}] = MFetching::model()->getCollection()->getDbRef($sid);
+    }
+    return $this->_seller_list[$sid['$id']->{'$id'}];
   }
 }
